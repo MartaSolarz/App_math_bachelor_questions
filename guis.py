c7b904c41106e523d991675d8c37a7ec29130818
msolarzwebsensa <msolarzwebsensa@dcwp.pl>
2023-10-18 15:04:45 +0100

    improve logic

--- guis.py
+++ guis.py
@@ -1,220 +1,73 @@
 import tkinter as tk
-
 from manager import Manager
 
-class Guis():
-    
+class Guis:
+
     def __init__(self, app, data) -> None:
         self.app = app
         self.questions = data
+        self.colors = {
+            "AM": 'red',
+            "GAL": 'red',
+            "WDM": 'red',
+            "WDI": 'red',
+            "RRZ": 'green',
+            "RP": 'green',
+            "TOPO": 'green',
+            "ALG": 'green',
+            "MO": 'green',
+            "SAD": 'blue',
+            "FUN": 'blue'
+        }
+
+    def createWindow(self, category, title_text):
+        new_window = tk.Toplevel(self.app)
+        new_window.title(title_text)
+
+        title = tk.Label(new_window, text=title_text, fg=self.colors[category])
+        title.pack()
+
+        questions = self.questions.choose_category(category)
+        manager = Manager(new_window, questions)
+
+        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)
+        exit_button.pack()
+
+        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
+        display_all_questions.pack()
+
+        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
+        start_learn.pack()
 
     def AM_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Analiza matematyczna")
-
-        title = tk.Label(new_window, text="Analiza matematyczna", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("AM")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("AM", "Analiza matematyczna")
 
     def GAL_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Geometria z algebrą liniową")
-
-        title = tk.Label(new_window, text="Geometria z algebrą liniową", fg='red')
-        title.pack()
-
-        questions = self.questions.choose_category("GAL")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
-
+        self.createWindow("GAL", "Geometria z algebrą liniową")
 
     def WDM_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Wstęp do matematyki")
-
-        title = tk.Label(new_window, text="Wstęp do matematyki", fg='blue')
-        title.pack()
-
-        questions = self.questions.choose_category("WDM")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
-
+        self.createWindow("WDM", "Wstęp do matematyki")
 
     def WDI_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Wstęp do informatyki")
-
-        title = tk.Label(new_window, text="Wstęp do informatyki", fg='purple')
-        title.pack()
-        
-        questions = self.questions.choose_category("WDI")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("WDI", "Wstęp do informatyki")
 
     def RRZ_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Równania różniczkowe zwyczajne")
-
-        title = tk.Label(new_window, text="Równania różniczkowe zwyczajne", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("RRZ")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()    
+        self.createWindow("RRZ", "Równania różniczkowe zwyczajne")
 
     def RP_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Rachunek prawdopodobieństwa")
-
-        title = tk.Label(new_window, text="Rachunek prawdopodobieństwa", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("RP")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("RP", "Rachunek prawdopodobieństwa")
 
     def TOPO_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Topologia")
-
-        title = tk.Label(new_window, text="Topologia", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("TOPO")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("TOPO", "Topologia")
 
     def ALG_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Algebra")
-
-        title = tk.Label(new_window, text="Algebra", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("ALG")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("ALG", "Algebra")
 
     def MO_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Matematyka obliczeniowa")
-
-        title = tk.Label(new_window, text="Matematyka obliczeniowa", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("MO")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("MO", "Matematyka obliczeniowa")
 
     def SAD_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Statystyczna analiza danych")
-
-        title = tk.Label(new_window, text="Statystyczna analiza danych", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("SAD")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("SAD", "Statystyczna analiza danych")
 
     def FUN_gui(self):
-        new_window = tk.Toplevel(self.app)
-        new_window.title("Funkcje analityczne")
-
-        title = tk.Label(new_window, text="Funkcje analityczne", fg='green')
-        title.pack()
-
-        questions = self.questions.choose_category("FUN")
-        manager = Manager(new_window, questions)
-
-        exit_button = tk.Button(new_window, text='Wyjście', width=25, command=new_window.destroy)    
-        exit_button.pack()
-
-        display_all_questions = tk.Button(new_window, text='Pokaż wszystkie pytania', width=25, command=manager.display_all_questions)
-        display_all_questions.pack()
-
-        start_learn = tk.Button(new_window, text="Zacznij naukę", width=25, command=manager.start_order_learn)
-        start_learn.pack()
+        self.createWindow("FUN", "Funkcje analityczne")
--- questions.py
+++ questions.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+FILE_WITH_QUESTIONS = r'D:/Marta/Documents/pytania_licencjat_matma.csv'
+
 class Questions:
     def __init__(self):
-        self.data = pd.read_csv(r'D:/Marta/Documents/pytania_licencjat_matma.csv', sep=';', index_col='Przedmiot')
+        self.data = pd.read_csv(FILE_WITH_QUESTIONS, sep=';', index_col='Przedmiot')
         self.data.columns = [i+1 for i in range(len(self.data.columns))]
 
     def get_category(self, name):
